ce2dcd47b6025ad36ff81030f167f7186c3195af
Vince Varga <vince.varga@smaho.com>
2019-03-17 21:12:11 +0100

    Add basic navigation between slides

---
+++ lib/example_slide_A.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExampleSlideA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Example Slide A"),
+    );
+  }
+}
---
+++ lib/example_slide_C.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExampleSlideC extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Example Slide C"),
+    );
+  }
+}
